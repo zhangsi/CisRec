4b654e8e83a127c48942b79e3d2d0ccf86190b9e
Si Zhang <zhangsi.cs@gmail.com>
2012-04-21 16:52:07 +0800

    add biased baseline model

---
+++ src/org/cis/cf/algorithm/BiasedBaseline.java
@@ -0,0 +1,99 @@
+package org.cis.cf.algorithm;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.cis.data.Ratings;
+
+
+public class BiasedBaseline implements RatingPredictor {
+	private Ratings ratings;
+	
+	private int userNumber;
+	private int itemNumber;
+	
+	private double globalBias;
+	
+	private double[] userBias;
+	private double[] itemBias;
+	
+	private double maxIterNumber;
+	private int trainNumber;
+	
+	private double learnRate;
+	
+	private int maxRating;
+	private int minRating;
+	
+	public BiasedBaseline(Ratings ratings, int maxIterNumber, double learnRate){
+		this.ratings = ratings;
+		this.userNumber = ratings.totalUserNumber();
+		this.itemNumber = ratings.totalItemNumber();
+		this.globalBias = ratings.averageRating();
+		this.maxIterNumber = maxIterNumber;
+		this.trainNumber  = ratings.getCount();
+		this.learnRate  = learnRate;
+		
+		this.maxRating = ratings.getMaxRating();
+		this.minRating = ratings.getMinRating();
+		
+		userBias = new double[userNumber + 1];
+		itemBias = new double[itemNumber + 1];
+	}
+	
+	private void initModel() {
+		Random rand = new Random();
+		for( int u = 0; u <= userNumber; ++u){
+			userBias[u] = rand.nextGaussian() * 0.01;
+		}
+		
+		for( int i = 0; i <= itemNumber; ++i){
+			itemBias[i] = rand.nextGaussian() * 0.01;
+		}
+	}
+	
+	@Override
+	public double predict(int user_id, int item_id, boolean bound){
+		
+		double result = userBias[user_id] + itemBias[item_id];
+		result += globalBias;
+		
+		if(bound){
+			if( result > maxRating)
+				result = (double) maxRating;
+			if( result < minRating)
+				result = (double) minRating;
+		}
+		return result;
+	}
+
+	@Override
+	public void trainModel() {
+		initModel();
+		learnBias();
+	}
+	
+	private void learnBias(){
+		for(int iter = 1; iter <= maxIterNumber; ++iter){
+			iterate(ratings.getRandomIndex());
+		}
+	}
+	
+	private void iterate(ArrayList<Integer> list){
+		int user_id, item_id, rating;
+		double prediction, gradient;
+		for(int index = 0; index != trainNumber; ++index){
+			
+			user_id = ratings.getUser(index);
+			item_id = ratings.getItem(index);
+			rating  = ratings.getRating(index);
+			
+			prediction = globalBias + userBias[user_id] + itemBias[item_id];
+			gradient = rating - prediction;
+			
+			userBias[user_id] += learnRate * learnRate * (gradient - learnRate  * userBias[user_id]);
+			itemBias[item_id] += learnRate * learnRate * (gradient - learnRate  * itemBias[item_id]);
+			
+		}
+	}
+}
---
+++ src/org/cis/cf/test/BiasedBaselineTest.java
@@ -0,0 +1,44 @@
+package org.cis.cf.test;
+
+import org.cis.cf.algorithm.BiasedBaseline;
+import org.cis.data.Ratings;
+import org.cis.eval.RmseEvaluator;
+import org.cis.io.MovielensRatingsReader;
+
+public class BiasedBaselineTest {
+	public static void main(String args[]){
+		
+		
+		//command: java org.cis.cf.test.BiasedBaselineTest ../data/movielens/u1.base ../data/movielens/u1.test 0.05 50
+		
+		
+		if(args.length != 4){
+			System.out.println("Useage:");
+			System.out.println("1, Training data path");
+			System.out.println("2, Test data path");
+			System.out.println("3, Learning rate");
+			System.out.println("4, Max round of training");
+		} else {
+			String trainFile = args[0];
+			String testFile = args[1];
+			
+			double learnRate = Double.parseDouble(args[2]);
+			int maxIter = Integer.parseInt(args[3]);
+			
+			MovielensRatingsReader reader = new MovielensRatingsReader();
+			Ratings trainData  = reader.read(trainFile);
+			Ratings testData   = reader.read(testFile);
+			
+			BiasedBaseline recommender = new BiasedBaseline(
+					trainData,
+					maxIter,
+					learnRate
+					);
+			
+			recommender.trainModel();
+			
+			RmseEvaluator evaluator = new RmseEvaluator();
+			System.out.println(evaluator.evaluate(recommender, testData));
+		}
+	}
+}
